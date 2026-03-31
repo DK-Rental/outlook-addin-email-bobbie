bcd029636272ad28f31c381a418d6a5b0eef42e9
tinaleung <tinaleungisworking@gmail.com>
2026-03-30 23:12:27 -0700

    testing

--- src/taskpane/config.js
+++ src/taskpane/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: "https://outlook-addin-backend-a9exd5cfg4a4b4ez.canadacentral-01.azurewebsites.net"
+    API_URL: "https://localhost:5000"
 };
 
 export default config;
